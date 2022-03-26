1455f21dea6354ef606acd51595975f5d3b5eebd
Erubin <magomedamin90@ya.ru>
2022-03-26 18:20:46 +0300

    Create project

---
+++ src/main/java/io/ylab/ticTacToeGameRestApi/TicTacToeGameRestApiApplication.java
@@ -0,0 +1,13 @@
+package io.ylab.ticTacToeGameRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeGameRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicTacToeGameRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/ylab/ticTacToeGameRestApi/TicTacToeGameRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ylab.ticTacToeGameRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeGameRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
