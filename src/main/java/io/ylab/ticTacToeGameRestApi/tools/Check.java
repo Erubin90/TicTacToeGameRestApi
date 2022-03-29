c608415a3b2214cff765d5e871bea2815b615dc3
Erubin <magomedamin90@ya.ru>
2022-03-30 02:19:48 +0300

    Удалены методы playerRequest, gameRequest, добавлены isNull, isNullOrEmpty

--- src/main/java/io/ylab/ticTacToeGameRestApi/tools/Check.java
+++ src/main/java/io/ylab/ticTacToeGameRestApi/tools/Check.java
@@ -1,24 +1,17 @@
 package io.ylab.ticTacToeGameRestApi.tools;
 
 import io.ylab.ticTacToeGameRestApi.excrptions.InvalidValueException;
-import io.ylab.ticTacToeGameRestApi.objects.requests.GameRequest;
-import io.ylab.ticTacToeGameRestApi.objects.requests.PlayerRequest;
 
 public class Check {
 
-    public static void playerRequest(PlayerRequest player) {
-        if (player != null) {
-            var playerId = player.getId();
-            if (playerId == null)
-                throw new InvalidValueException("player_id == null");
-        }
-        else
-            throw new InvalidValueException("player is null");
+    public static void isNull(Object o, String nameParam) {
+        if (o == null)
+            throw new InvalidValueException(nameParam + " is null");
     }
 
-    public static void gameRequest(GameRequest game) {
-
+    public static void isNullOrEmpty(String string, String nameParam) {
+        isNull(string, nameParam);
+        if (string.isEmpty())
+            throw new InvalidValueException(nameParam + "is empty");
     }
-
-
 }
