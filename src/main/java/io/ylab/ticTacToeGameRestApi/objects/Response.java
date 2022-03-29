c696f1fb28a56b06a37d7dd7a13c377eea16ee76
Erubin <magomedamin90@ya.ru>
2022-03-30 02:10:40 +0300

    Обновил класс Response

--- src/main/java/io/ylab/ticTacToeGameRestApi/objects/Response.java
+++ src/main/java/io/ylab/ticTacToeGameRestApi/objects/Response.java
@@ -2,21 +2,23 @@ package io.ylab.ticTacToeGameRestApi.objects;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonPropertyOrder(value = {"message", "error"})
+@JsonPropertyOrder(value = {"message", "errorCode"})
 public class Response<T> {
 
     @JsonProperty(value = "message")
     private T message;
 
-    @JsonProperty(value = "error")
-    private Error error;
+    @JsonProperty(value = "errorCode")
+    private Integer error;
+
+    public Response(T message) {
+        this.message = message;
+    }
 }
