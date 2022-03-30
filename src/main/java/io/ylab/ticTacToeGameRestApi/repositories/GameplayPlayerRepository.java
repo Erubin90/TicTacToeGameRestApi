97c0ef02a96fc90e027e04678e875136d032f8d1
Erubin <magomedamin90@ya.ru>
2022-03-30 05:26:23 +0300

    Удален метод findByGameplayIsAndPlayerIs

--- src/main/java/io/ylab/ticTacToeGameRestApi/repositories/GameplayPlayerRepository.java
+++ src/main/java/io/ylab/ticTacToeGameRestApi/repositories/GameplayPlayerRepository.java
@@ -1,8 +1,6 @@
 package io.ylab.ticTacToeGameRestApi.repositories;
 
-import io.ylab.ticTacToeGameRestApi.entities.Gameplay;
 import io.ylab.ticTacToeGameRestApi.entities.GameplayPlayer;
-import io.ylab.ticTacToeGameRestApi.entities.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,8 +9,6 @@ import java.util.List;
 
 @Repository
 public interface GameplayPlayerRepository extends JpaRepository<GameplayPlayer, Long> {
-
-    GameplayPlayer findByGameplayIsAndPlayerIs(Gameplay gameplay, Player player);
 
     @Query("FROM GameplayPlayer WHERE player.id = :playerId")
     List<GameplayPlayer> findAllByPlayerId(Long playerId);
