6354134c6755924d4246efad31aa9fe3a6df0aa9
Erubin <magomedamin90@ya.ru>
2022-03-30 06:38:35 +0300

    Написал реализацию метода replayGameplayById

--- src/main/java/io/ylab/ticTacToeGameRestApi/controllers/GameplayController.java
+++ src/main/java/io/ylab/ticTacToeGameRestApi/controllers/GameplayController.java
@@ -2,10 +2,10 @@ package io.ylab.ticTacToeGameRestApi.controllers;
 
 import io.ylab.ticTacToeGameRestApi.objects.Response;
 import io.ylab.ticTacToeGameRestApi.objects.json.GameplayJson;
+import io.ylab.ticTacToeGameRestApi.objects.simulation.SimulationGame;
 import io.ylab.ticTacToeGameRestApi.services.GameplayService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -35,8 +35,12 @@ public class GameplayController {
     }
 
     @GetMapping("/gameplay/{id}/replay")
-    public void replayGameplayById(@PathVariable Long id) {
-
+    public Response<String> replayGameplayById(@PathVariable Long id) {
+        var gameplay = service.getGameplay(id);
+        var gameplayJson = new GameplayJson(gameplay);
+        SimulationGame.play(gameplayJson);
+        var response = new Response<>("The game has been successfully played");
+        return response;
     }
 
     @PostMapping("/gameplay")
@@ -55,8 +59,9 @@ public class GameplayController {
         return response;
     }
 
-    @PostMapping("/gameplay/replay")
-    public void replayGameplayByFile(@RequestBody MultipartFile multipartFile) {
-
-    }
+//    @PostMapping("/gameplay/replay")
+//    public Response<String> replayGameplayByFile(@RequestBody MultipartFile multipartFile) {
+//        var response = new Response<>("The game has been successfully played");
+//        return response;
+//    }
 }
