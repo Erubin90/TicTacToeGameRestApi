84c0cf830aafc9eee5c929c191a513d8d47ab28c
Erubin <magomedamin90@ya.ru>
2022-04-03 15:46:39 +0300

    Добавил перечисление поддерживаемых файлов для парсинга

---
+++ src/main/java/io/ylab/ticTacToeGameRestApi/utils/enums/FileFormat.java
@@ -0,0 +1,31 @@
+package io.ylab.ticTacToeGameRestApi.utils.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum FileFormat {
+    XML("xml"),
+    JSON("json"),
+    NO_CORRECT("");
+
+    private final String format;
+
+    public static FileFormat getFileFormat(String format) {
+        if (JSON.format.equals(format)) {
+            return JSON;
+        }
+        else if (XML.format.equals(format)) {
+            return XML;
+        }
+        else {
+            return NO_CORRECT;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return format;
+    }
+}
