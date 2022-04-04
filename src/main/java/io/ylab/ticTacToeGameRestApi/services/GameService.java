a3cd233caf0d540ae2c8b0001699217147fae93e
Erubin <magomedamin90@ya.ru>
2022-04-04 21:59:34 +0300

    Добавил метод get

--- src/main/java/io/ylab/ticTacToeGameRestApi/services/GameService.java
+++ src/main/java/io/ylab/ticTacToeGameRestApi/services/GameService.java
@@ -4,6 +4,8 @@ import io.ylab.ticTacToeGameRestApi.model.Game;
 
 public interface GameService {
 
+    Game get(Long id);
+
     Game save(Game game);
 
     Game create(int bordSize, int amountSymbolLine, int typeGame);
