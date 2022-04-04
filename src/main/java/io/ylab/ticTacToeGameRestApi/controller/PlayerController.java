e467d335986d373fd0495d1102763fce692b2ea6
Erubin <magomedamin90@ya.ru>
2022-04-04 14:57:24 +0300

    Добавил проверки на null во все методы

--- src/main/java/io/ylab/ticTacToeGameRestApi/controller/PlayerController.java
+++ src/main/java/io/ylab/ticTacToeGameRestApi/controller/PlayerController.java
@@ -3,6 +3,7 @@ package io.ylab.ticTacToeGameRestApi.controller;
 import io.ylab.ticTacToeGameRestApi.dto.Response;
 import io.ylab.ticTacToeGameRestApi.dto.PlayerDto;
 import io.ylab.ticTacToeGameRestApi.services.PlayerService;
+import io.ylab.ticTacToeGameRestApi.utils.Check;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,22 +15,25 @@ public class PlayerController {
 
     @GetMapping(value = "/players/{id}")
     public Response<PlayerDto> getPlayer(@PathVariable Long id) {
-        var player = playerService.getPlayer(id);
+        Check.isNull(id, "id");
+        var player = playerService.get(id);
         var playerJson = new PlayerDto(player);
         return new Response<>(playerJson);
     }
 
     @PostMapping(value = "/players")
-    public Response<PlayerDto> createPlayer(@RequestBody PlayerDto player) {
-        var createPlayer = playerService.savePlayer(player.getPlayer());
-        player.setPlayer(createPlayer);
-        return new Response<>(player);
+    public Response<PlayerDto> createPlayer(@RequestBody PlayerDto request) {
+        Check.isNull(request, "request");
+        var createPlayer = playerService.save(request.getPlayer());
+        request.setPlayer(createPlayer);
+        return new Response<>(request);
     }
 
     @PutMapping(value = "/players")
-    public Response<PlayerDto> updatePlayer(@RequestBody PlayerDto player) {
-        var updatePlayer = playerService.savePlayer(player.getPlayer());
-        player.setPlayer(updatePlayer);
-        return new Response<>(player);
+    public Response<PlayerDto> updatePlayer(@RequestBody PlayerDto request) {
+        Check.isNull(request, "request");
+        var updatePlayer = playerService.update(request.getPlayer());
+        request.setPlayer(updatePlayer);
+        return new Response<>(request);
     }
 }
